1cd7d7c78b6453cb30ee88394cefb7ec28f468be
Hayden <18362975922@189.cn>
2020-05-02 16:19:19 +0800

    feat

---
+++ store.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const COVER_URL = [
+  require('./src/assets/cover.jpg'),
+  require('./src/assets/cover2.jpg'),
+  require('./src/assets/cover3.jpg'),
+]
+
+export default new Vuex.Store({
+  state: {
+    isPlaying: false,
+    coverUrl: '',
+  },
+  getters: {},
+  mutations: {
+    togglePlay(state, toggle) {
+      state.isPlaying = toggle !== undefined ? toggle : !state.isPlaying
+    },
+    changeCover(state) {
+      let t = true
+      while (t) {
+        const index = Math.floor(Math.random() * 3)
+        const coverUrl = COVER_URL[index]
+        if (coverUrl !== state.coverUrl) {
+          state.coverUrl = coverUrl
+          break
+        }
+      }
+    },
+  },
+  actions: {},
+})
